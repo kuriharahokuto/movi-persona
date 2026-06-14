be6b014f26e8b48187729b081662be1ae3b7cd4b
栗原北斗 <martnight1011@gmail.com>
2026-06-14 15:02:17 +0900

    Update share function, modified index,src/main

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+if (window.location.pathname !== "/" || window.location.search || window.location.hash) {
+  window.history.replaceState({}, "", "/");
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
